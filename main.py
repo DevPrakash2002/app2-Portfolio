909bbc551f16db8f411ab5e483306067f00661f9
Dev Prakash <dev.tiwary5821@gmail.com>
2023-04-24 23:41:35 +0530

    4th commit (changes in web app like integrating columns and working on the mages and adding links)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 
 st.set_page_config(layout="wide")
@@ -15,3 +16,26 @@ with col2:
     """
 
     st.info(about_content)
+
+Content2 = """
+Below you can find some of apps i have built as python developer or a android Developer
+"""
+
+st.write(Content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/"+row['image'])
+        st.write(f'[Source Code]({row["url"]})')
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f'[Source Code]({row["url"]})')
